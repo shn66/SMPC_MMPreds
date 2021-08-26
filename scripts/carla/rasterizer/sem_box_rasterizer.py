75d3bf54945a154e1285721cd19ae3d8566abe7d
Vijay Govindarajan <govvijay@berkeley.edu>
2021-08-26 10:27:41 -0700

    Moving option to render traffic lights in SemBoxRasterizer to constructor rather than rasterize method.

--- scripts/carla/rasterizer/sem_box_rasterizer.py
+++ scripts/carla/rasterizer/sem_box_rasterizer.py
@@ -6,6 +6,7 @@ from .box_rasterizer import BoxRasterizer
 class SemBoxRasterizer:
     def __init__(self,
                  carla_topology,
+                 render_traffic_lights=True,        # whether to overlay traffic lights on the image
                  raster_size=(500, 500),            # pixel height and width
                  raster_resolution=0.1,             # meters / pixel resolution
                  target_center_x = 100,             # pixels along x-axis where target center should be located
@@ -25,10 +26,11 @@ class SemBoxRasterizer:
                                       target_center_y = target_center_y,
                                       history_secs=history_secs,
                                       closeness_eps=closeness_eps)
+        self.render_traffic_lights = render_traffic_lights
 
-    def rasterize(self, agent_history, target_agent_id, render_traffic_lights=True):
+    def rasterize(self, agent_history, target_agent_id):
 
-        im_sem = self.sem_rast.rasterize(agent_history, target_agent_id, render_traffic_lights=render_traffic_lights)
+        im_sem = self.sem_rast.rasterize(agent_history, target_agent_id, self.render_traffic_lights)
         im_box = self.box_rast.rasterize(agent_history, target_agent_id)
 
         mask_box = np.any(im_box > 0, -1)     # which pixel locations have nonzero values in im_box
